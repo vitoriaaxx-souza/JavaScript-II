9fc9334ea895ef63bf9e02787424eabc41c28a1b
vitoriaaxx-souza <fernanda.vitoria.rodrigues@escola.pr.gov.br>
2026-04-23 14:42:18 +0000

    atividade 1 finalizada

---
+++ aula-1/Arrays.js
@@ -0,0 +1,51 @@
+const edDiamante = [
+  {
+  titulo: "O Homem em Busca de Sentido",
+  preco: 50
+  },
+  {
+    titulo: "Sapiens: Uma Breve História da Humanidade",
+    preco: 190
+  },
+  {
+    titulo: "O Guia do Mochileiro das Galáxias",
+    preco: 110
+  },
+  {
+    titulo: "Duna",
+    preco: 95
+  },
+  {
+    titulo: "O Sol é para Todos",
+    preco: 87
+  }
+]
+
+const edEsmeralda = [
+  {
+    titulo: "Cem Anos de Solidão",
+    preco: 42
+  },
+  {
+    titulo: "O Hobbit",
+    preco: 90
+  },
+  {
+    titulo: "A Hora da Estrela",
+    preco: 67
+  },
+  {
+    titulo: "Torto Arado",
+    preco: 45
+  },
+  {
+    titulo: "Dom Casmurro",
+    preco: 110
+  },
+  {
+    titulo: "A Amiga Genial",
+    preco: 34
+  }
+]
+
+module.exports = { edDiamante, edEsmeralda }
